0125042c1fda11665a1a51e74907fd4e139c8c0a
QuickSilver-1 <99834939+QuickSilver-1@users.noreply.github.com>
2025-02-17 07:17:49 +0300

    Update transaction.go

--- internal/presentation/realization/transaction.go
+++ internal/presentation/realization/transaction.go
@@ -17,119 +17,146 @@ type Transaction struct{}
 
 // NewTransaction создает новый экземпляр Transaction
 func NewTransaction() *Transaction {
-	return &Transaction{}
+    return &Transaction{}
 }
 
 // Transfer выполняет перевод средств
 func (s *Transaction) Transfer(transaction domain.Transaction) error {
-	LoggerService.Debug("Transferring")
+    LoggerService.Debug("Transferring")
 
-	tx, err := postgres.DbService.Db.Begin()
-	if err != nil {
-		return createTransactionError(err)
-	}
+    tx, err := postgres.DbService.Db.Begin()
+    if err != nil {
+        return createTransactionError(err)
+    }
 
-	if err := s.updateUserCoins(tx, transaction.ReceiverName, transaction.Amount); err != nil {
-		return s.rollbackTransaction(tx, err)
-	}
+    // Проверка наличия пользователя получателя
+    receiverExists, err := s.userExists(tx, transaction.ReceiverName)
+    if err != nil {
+        return s.rollbackTransaction(tx, err)
+    }
+    if !receiverExists {
+        err = tx.Rollback()
+        if err != nil {
+            LoggerService.Error(fmt.Sprintf("Rollback error: %v", err))
+        }
+        return &e.TransactionError{
+            Code: http.StatusBadRequest,
+            Err:  "Receiver does not exist",
+        }
+    }
 
-	if err := s.updateUserCoins(tx, transaction.SenderName, -transaction.Amount); err != nil {
-		return s.rollbackTransaction(tx, err)
-	}
+    if err := s.updateUserCoins(tx, transaction.ReceiverName, transaction.Amount); err != nil {
+        return s.rollbackTransaction(tx, err)
+    }
 
-	if err := s.insertTransaction(tx, transaction); err != nil {
-		return s.rollbackTransaction(tx, err)
-	}
+    if err := s.updateUserCoins(tx, transaction.SenderName, -transaction.Amount); err != nil {
+        return s.rollbackTransaction(tx, err)
+    }
 
-	if err := tx.Commit(); err != nil {
-		return createCommitError(err)
-	}
+    if err := s.insertTransaction(tx, transaction); err != nil {
+        return s.rollbackTransaction(tx, err)
+    }
 
-	return nil
+    if err := tx.Commit(); err != nil {
+        return createCommitError(err)
+    }
+
+    return nil
 }
 
 func (s *Transaction) updateUserCoins(tx *sql.Tx, email string, amount int) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-	defer cancel()
-	_, err := tx.ExecContext(ctx, `UPDATE Users SET "coins" = "coins" + $1 WHERE "email" = $2`, amount, email)
-	if err != nil {
-		return createDbQueryError(err)
-	}
-	return nil
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+    defer cancel()
+    _, err := tx.ExecContext(ctx, `UPDATE Users SET "coins" = "coins" + $1 WHERE "email" = $2`, amount, email)
+    if err != nil {
+        return createDbQueryError(err)
+    }
+    return nil
 }
 
 func (s *Transaction) insertTransaction(tx *sql.Tx, transaction domain.Transaction) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-	defer cancel()
-	_, err := tx.ExecContext(ctx, `INSERT INTO Transaction("sender_name", "receiver_name", "amount") VALUES ($1, $2, $3)`, transaction.SenderName, transaction.ReceiverName, transaction.Amount)
-	if err != nil {
-		return createDbQueryError(err)
-	}
-	return nil
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+    defer cancel()
+    _, err := tx.ExecContext(ctx, `INSERT INTO Transaction("sender_name", "receiver_name", "amount") VALUES ($1, $2, $3)`, transaction.SenderName, transaction.ReceiverName, transaction.Amount)
+    if err != nil {
+        return createDbQueryError(err)
+    }
+    return nil
 }
 
 func (s *Transaction) rollbackTransaction(tx *sql.Tx, originalErr error) error {
-	if rbErr := tx.Rollback(); rbErr != nil {
-		LoggerService.Error(fmt.Sprintf("Rollback error: %v", rbErr))
-	}
-	return originalErr
+    if rbErr := tx.Rollback(); rbErr != nil {
+        LoggerService.Error(fmt.Sprintf("Rollback error: %v", rbErr))
+    }
+    return originalErr
+}
+
+func (s *Transaction) userExists(tx *sql.Tx, email string) (bool, error) {
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+    defer cancel()
+    var exists bool
+    err := tx.QueryRowContext(ctx, `SELECT EXISTS(SELECT 1 FROM Users WHERE "email" = $1)`, email).Scan(&exists)
+    if err != nil {
+        return false, createDbQueryError(err)
+    }
+    return exists, nil
 }
 
 func createTransactionError(err error) error {
-	return &e.TransactionError{
-		Code: http.StatusInternalServerError,
-		Err:  fmt.Sprintf("Creating transaction error: %v", err),
-	}
+    return &e.TransactionError{
+        Code: http.StatusInternalServerError,
+        Err:  fmt.Sprintf("Creating transaction error: %v", err),
+    }
 }
 
 func createDbQueryError(err error) error {
-	return &e.DbQueryError{
-		Code: http.StatusInternalServerError,
-		Err:  fmt.Sprintf("Db query error: %v", err),
-	}
+    return &e.DbQueryError{
+        Code: http.StatusInternalServerError,
+        Err:  fmt.Sprintf("Db query error: %v", err),
+    }
 }
 
 func createCommitError(err error) error {
-	return &e.TransactionError{
-		Code: http.StatusInternalServerError,
-		Err:  fmt.Sprintf("Commit error: %v", err),
-	}
+    return &e.TransactionError{
+        Code: http.StatusInternalServerError,
+        Err:  fmt.Sprintf("Commit error: %v", err),
+    }
 }
 
 // GetTransaction получает транзакции для указанного пользователя по email
 func (s *Transaction) GetTransaction(user domain.UserEmail) (*[]domain.Transaction, error) {
-	LoggerService.Debug("Getting transaction")
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-	defer cancel()
-	rows, err := postgres.DbService.Db.QueryContext(ctx, `SELECT "id", "sender_name", "receiver_name", "amount" FROM Transaction WHERE "sender_name" = $1 OR "receiver_name" = $1`, user)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return nil, nil
-		}
-		return nil, &e.DbQueryError{
-			Code: http.StatusInternalServerError,
-			Err:  fmt.Sprintf("Query db error: %v", err),
-		}
-	}
-	defer func() {
-		err := rows.Close()
-		if err != nil {
-			LoggerService.Error(fmt.Sprintf("Error closing rows: %v", err))
-		}
-	}()
+    LoggerService.Debug("Getting transaction")
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+    defer cancel()
+    rows, err := postgres.DbService.Db.QueryContext(ctx, `SELECT "id", "sender_name", "receiver_name", "amount" FROM Transaction WHERE "sender_name" = $1 OR "receiver_name" = $1`, user)
+    if err != nil {
+        if errors.Is(err, sql.ErrNoRows) {
+            return nil, nil
+        }
+        return nil, &e.DbQueryError{
+            Code: http.StatusInternalServerError,
+            Err:  fmt.Sprintf("Query db error: %v", err),
+        }
+    }
+    defer func() {
+        err := rows.Close()
+        if err != nil {
+            LoggerService.Error(fmt.Sprintf("Error closing rows: %v", err))
+        }
+    }()
 
-	var transactions []domain.Transaction
-	for rows.Next() {
-		var transaction domain.Transaction
-		err = rows.Scan(&transaction.Id, &transaction.SenderName, &transaction.ReceiverName, &transaction.Amount)
-		if err != nil {
-			return nil, &e.DbQueryError{
-				Code: http.StatusInternalServerError,
-				Err:  fmt.Sprintf("Scanning subject error: %v", err),
-			}
-		}
-		transactions = append(transactions, transaction)
-	}
+    var transactions []domain.Transaction
+    for rows.Next() {
+        var transaction domain.Transaction
+        err = rows.Scan(&transaction.Id, &transaction.SenderName, &transaction.ReceiverName, &transaction.Amount)
+        if err != nil {
+            return nil, &e.DbQueryError{
+                Code: http.StatusInternalServerError,
+                Err:  fmt.Sprintf("Scanning subject error: %v", err),
+            }
+        }
+        transactions = append(transactions, transaction)
+    }
 
-	return &transactions, nil
+    return &transactions, nil
 }
